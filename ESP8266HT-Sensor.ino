5c46f028509cb96cc241b5a1bb56480828fde5d5
Stefan Sievert <stefan.sievert@stud-informatik.de>
2018-12-13 18:23:35 +0100

    Sensoren simplyfied

--- ESP8266HT-Sensor.ino
+++ ESP8266HT-Sensor.ino
@@ -1,84 +1,74 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 #include <Wire.h>
 #include "Seeed_BME280.h"
 
 void setup();
 void loop();
-void getTempAndHum();
-void getSensorInfo();
-void notFound();
-void goToDeepSleep();
 
 //WiFi Connection configuration
-const char *ssid = "ESPMasterController";
-const char *password = "[the secure password]";
+const char *ssid = "ESP32Master";
+const char *password = "[password of the master]";
 
-ESP8266WebServer server(80);
+HTTPClient http;
 BME280 sensor;
-static bool goToDS = false;
-static int updatePeriod = 2 * 1000;
-static unsigned long time_now = 0;
+const int bufferSize = 36;
+static char buff[bufferSize];
 
 void setup() {
-  delay(1000);
-  Serial.begin(115200);
-  Serial.println();
+	Serial.begin(115200);
+	Serial.println();
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);    
-  Serial.println("");
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	Serial.println("");
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP()); 
-  
-  server.on("/getTempAndHum", getTempAndHum); 
-  server.on("/getSensorInfo", getSensorInfo); 
-  server.onNotFound(notFound);
-  server.begin();
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+	sensor.init();
 
-  sensor.init();
+	delay(1000);
+	sendTempAndHum();
 }
 
 void loop() {
-  server.handleClient();
-  if (!goToDS) {
-	  time_now = millis() + updatePeriod + 1000;
-  }
-  if (millis() > time_now + updatePeriod && goToDS) {
-	  time_now = millis();
-	  goToDeepSleep();
-  }
-  if (millis() > 60 * 1000) {
-	  Serial.println("To long awake, restart!");
-	  ESP.restart();
-  }
 
 }
 
-void getTempAndHum() {
-  server.send(200, "text/json", String() +   F("{\"Intern\": {\"temperature\": ") + String(sensor.getTemperature()) +  F(", \"humidity\": ") + String(sensor.getHumidity()) + F(", \"pressure\": ") + String((float)sensor.getPressure() / (float)100.00) + F("}}"));
-  goToDS = true;
+void sendTempAndHum() {
+	float t = sensor.getTemperature();
+	float p = (float)(sensor.getPressure()) / (float)100.00;
+	float h = sensor.getHumidity();
+	http.begin("http://" + WiFi.gatewayIP().toString() + "/payload");      //Specify request destination
+	String payload = "MAC=" + WiFi.macAddress() + "&Version=v2&Payload=T_" + String(t) + "H_" + String(h) + "P_" + String(p);
+	Serial.println("Try to send to: http://" + WiFi.gatewayIP().toString() + "/payload");
+	Serial.println("payload: " + payload);
+	Serial.println("Content-Length: " + String(payload.length()));
+	http.addHeader("Content-Type", "text/plain");
+	http.addHeader("Content-Length", String(payload.length()));
+	int httpCode = http.POST(payload);
+	String get = http.getString();                  //Get the response payload
+	Serial.println(httpCode);   //Print HTTP return code
+	Serial.println(get);    //Print request response payload
+	http.end();  //Close connection
+	if (httpCode == 200) {
+		goToDeepSleep(10);
+	}
+	else {
+		goToDeepSleep(2);
+	}
 }
 
-void getSensorInfo() {
-  server.send(200, "text/json", String() +   F("{\"function\": \"getTempAndHum\", \"version\": \"v2\"}"));
-}
-
-void notFound(){
-  server.send(404, "text/plain", "404 Not Found");
-}
-
-void goToDeepSleep(){
-  Serial.println("Data send, going to Deep Sleep");
-  delay(1);
-  ESP.deepSleep(1000000*60*10); // 10 Minutes Deep Sleep
+void goToDeepSleep(int min) {
+	Serial.println("Data send, going to Deep Sleep");
+	delay(1);
+	ESP.deepSleep(1000000 * 60 * min); // 10 Minutes Deep Sleep
 }
